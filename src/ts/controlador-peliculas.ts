d10883d7f10110a6166ac633492469caab6c7980
Ovidiu Muresan <osilvium@gmail.com>
2020-10-23 00:07:47 +0200

    Class ControladorPeliculas & Class VistaPeliculas

--- src/ts/controlador-peliculas.ts
+++ src/ts/controlador-peliculas.ts
@@ -1,1 +1,35 @@
-/* Aquí deberás crear la clase para el controlador de películas, que se encargará de extraer datos y realizar operaciones sobre el conjunto de las películas */
+import {Pelicula} from "./datos/pelicula";
+import * as BBDD from "./peliculas.json";
+
+export class ControladorPeliculas {
+  private peliculas: Pelicula[] = [];
+  private peliculasVistas: Pelicula[] = [];
+  private peliculasPendientes: Pelicula[] = [];
+
+  constructor() { }
+
+  public almacenarPeliculas(): void {
+    for (const pelicula of BBDD.peliculas) {
+      const nuevaPelicula: Pelicula = new Pelicula(
+        pelicula.id,
+        pelicula.titulo,
+        pelicula.director,
+        pelicula.fecha,
+        pelicula.cartel,
+        pelicula.vista,
+        pelicula.formato,
+        pelicula.valoracion,
+        pelicula.oscars,
+      );
+      this.peliculas.push(nuevaPelicula);
+    }
+    this.separarPeliculas();
+    console.log(this.peliculasPendientes);
+  }
+
+  private separarPeliculas(): void {
+    this.peliculasVistas = this.peliculas.filter(p => p.vista === true);
+    this.peliculasPendientes = this.peliculas.filter(p => p.vista === false);
+  }
+
+}
--- src/ts/vista-peliculas.ts
+++ src/ts/vista-peliculas.ts
@@ -1,1 +1,23 @@
-/* Aquí deberás crear la clase para el controlador de la vista, que será el que se comunique directamente con el HTML */
+import { ControladorPeliculas } from "./controlador-peliculas";
+export class VistaPeliculas {
+  private HTML: any = {};
+
+  constructor(private CFacturas: ControladorPeliculas) {
+    this.alamcenarHtml();
+  }
+
+  public alamcenarHtml(): void {
+    this.HTML.listaPendientes = document.querySelector(".js-lista-pendientes");
+    this.HTML.listaVistas = document.querySelector(".js-lista-vistas");
+    this.HTML.peliculasVistas = this.HTML.listaVistas.querySelectorAll("li.js-pelicula");
+    this.HTML.peliculasPendiente = this.HTML.listaPendientes.querySelectorAll("li.js-pelicula");
+    console.log(this.HTML);
+  }
+
+  public limpiarListado(pendiente: boolean): void {
+    const listado = pendiente === true ? this.HTML.peliculasPendiente : this.HTML.peliculasVistas;
+    for (const elemento of listado) {
+      elemento.remove();
+    }
+  }
+}
